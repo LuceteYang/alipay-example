2e851650bbf64c9270ab091133339e2565c61cea
sanghwan <jae6120@naver.com>
2018-08-30 14:08:31 +0900

    refund 환불 처리 완료

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,11 @@ router.get('/', function(req, res, next) {
 router.get('/wappay/pay', function(req, res, next) {
   res.render('pay');
 });
+router.get('/wappay/refund', function(req, res, next) {
+  res.render('refund');
+});
+
+
 
 router.post('/wappay/pay', function(req, res, next) {
     var out_trade_no = req.body.WIDout_trade_no;
@@ -37,12 +42,31 @@ router.post('/wappay/pay', function(req, res, next) {
     var url = "https://openapi.alipay.com/gateway.do" + '?'+params;
     res.redirect(url);
 });
+router.post('/wappay/refund', function(req, res, next) {
+    var out_trade_no = req.body.WIDout_trade_no;
+    var trade_no = req.body.WIDtrade_no;
+    var refund_amount = req.body.WIDrefund_amount;
+    var refund_reason = req.body.WIDrefund_reason;
+    var out_request_no = req.body.WIDout_request_no;
+    ali.refund({
+        outTradeId:out_trade_no,
+        tradeId:trade_no,
+        refundAmount:refund_amount,
+        refundReason:refund_reason,
+        outRequestId:out_request_no
+    }).then(function(value) {
+        console.log('동신 성공 ',value.response.statusCode===200);
+        console.log('환불 성공 ',JSON.parse(value.response.body).alipay_trade_refund_response.code==="10000");
+        res.send(value);
+    });
+});
 
 router.get('/callback/alipay', function(req, res, next) {
     res.render('return');
 })
 
-router.post('/callback/alipay', function(req, res, next) {
+router.post('/notify/alipay', function(req, res, next) {
+    //결제 등록처리
     console.log(req.url,req.body);
     res.send();
 })
